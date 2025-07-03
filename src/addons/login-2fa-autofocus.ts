fe237e12162f075711d7c9d3498f474edb2e0723
Keve Lajsz <kevelajsz@icloud.com>
2025-07-03 13:54:20 +0200

    change addon exporting method

--- src/addons/login-2fa-autofocus.ts
+++ src/addons/login-2fa-autofocus.ts
@@ -3,15 +3,17 @@ import { when } from "@/pubsub";
 import { isOnLoginPage } from "@/pubsub/pathname";
 import { elementVisibleInDOM } from "@/pubsub/dom";
 
-export default {
-    name: "login-2fa-autofocus",
-    initialise() {
-        const twoFACodeInput = () => {
-            return $("neptun-two-factor-dialog-content #two-factor-qr-code-input-form-input");
-        };
+export default function login2faAutofocus(): Addon {
+    return {
+        name: "login-2fa-autofocus",
+        initialise() {
+            const twoFACodeInput = () => {
+                return $("neptun-two-factor-dialog-content #two-factor-qr-code-input-form-input");
+            };
 
-        when(isOnLoginPage(), elementVisibleInDOM(twoFACodeInput)).execute(() => {
-            twoFACodeInput().trigger("focus");
-        });
-    },
-} satisfies Addon;
+            when(isOnLoginPage(), elementVisibleInDOM(twoFACodeInput)).execute(() => {
+                twoFACodeInput().trigger("focus");
+            });
+        },
+    };
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import type { Addon } from "@/addons";
 import login2faAutofocus from "@/addons/login-2fa-autofocus.ts";
-import loginAutologin from "@/addons/login-autologin.ts";
+import loginSavedUsers from "@/addons/login-saved-users.ts";
 
-const addons: Addon[] = [login2faAutofocus, loginAutologin];
+const addons: Addon[] = [login2faAutofocus(), loginSavedUsers()];
 
 for (const addon of addons) {
     try {
